05ea3c0f3ec5b34e48d61b39ee96e137d3d5d916
Semih Eseroğlu <42324952+semih010999@users.noreply.github.com>
2022-11-14 20:13:18 +0300

    Add files via upload

---
+++ week3homework/week3homework/Program.cs
@@ -0,0 +1,64 @@
+﻿using week3homework;
+
+public class Program
+{
+    public static void Main()
+    {
+        ShoppingCart cart = new ShoppingCart();
+
+        cart.Add(new CartItem()
+        {
+            CartId = 1,
+            Product = new Product()
+            {
+                ProductId = 0,
+                ProductName = "Pro01",
+                Price = 50
+            },
+            Quantity = 5
+        });
+
+        cart.Add(new CartItem()
+        {
+            CartId = 2,
+            Product = new Product()
+            {
+                ProductId = 1,
+                ProductName = "Pro02",
+                Price = 70
+            },
+            Quantity = 14
+        });
+
+        cart.Add(new CartItem()
+        {
+            CartId = 3,
+            Product = new Product()
+            {
+                ProductId = 2,
+                ProductName = "Pro03",
+                Price = 100
+            },
+            Quantity = 19
+        });
+
+        var list = cart.GetList();
+        foreach (var item in list)
+            Console.WriteLine($"{item.CartId} " +
+                $"{item.Product.ProductName} " +
+                $"{item.Product.Price} " +
+                $"{item.Quantity}");
+
+        Console.WriteLine(cart.GetBalance(0));
+
+        Console.WriteLine(cart.Remove(1).Product.ProductName + " " + "removed");
+
+        list = cart.GetList();
+
+        foreach (var item in list)
+            Console.WriteLine($"{item.CartId} " +
+                $"{item.Product.ProductName} " +
+                $"{item.Product.Price} " +
+                $"{item.Quantity}");
+    }
+}
---
+++ week3homework/week3homework/ShoppingCart.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week3homework
+{
+    public class ShoppingCart
+    {
+        private List<CartItem> cartList;
+
+        public ShoppingCart()
+        {
+            cartList = new List<CartItem>();
+        }
+
+
+        // POST Create
+        public void Add(CartItem item)
+        {
+            cartList.Add(item);
+            Console.WriteLine($"Item added! {item.Product.ProductName}");
+        }
+
+        // GET/id GetBalance
+        public decimal GetBalance(int id)
+        {
+            return (decimal)(cartList[id].Quantity * cartList[id].Product.Price);
+        }
+
+        // GET
+        public List<CartItem> GetList()
+        {
+            return cartList;
+        }
+
+        // DELETE
+        public CartItem Remove(int id)
+        {
+            var cartItem = cartList.Where(cart => cart.CartId == id).First();
+            if (cartItem == null)
+                throw new Exception("Item not found!");
+            cartList.Remove(cartItem);
+            return cartItem;
+        }
+    }
+}
